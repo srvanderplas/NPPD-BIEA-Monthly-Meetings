1957cda3edacea568adfa2533758778e462d8aeb
Susan VanderPlas <srvanderplas@gmail.com>
2018-01-02 12:35:12 -0600

    Add github API info

--- Working Session 2018-01-03/API Code.R
+++ Working Session 2018-01-03/API Code.R
@@ -1,3 +1,20 @@
+# --- Setup --------------------------------------------------------------------
+pkgs <- installed.packages()
+
+require_pkgs <- c("dplyr", "tidyr", "magrittr", "lubridate", "stringr", "ggplot2", 
+              "jsonlite", "httr", "Quandl", "httpuv")
+
+pkgs_to_install <- require_pkgs[!require_pkgs %in% pkgs]
+
+if (length(pkgs_to_install) > 0) {
+  install.packages(pkgs_to_install)
+}
+
+# Clean up
+rm(pkgs, require_pkgs, pkgs_to_install)
+
+# Load packages
+
 # General data libraries
 library(dplyr) # Working with data
 library(tidyr) # Reshaping data
@@ -7,6 +24,7 @@ library(stringr) # string manipulation
 
 # JSON/API libraries
 library(jsonlite) # fromJSON function
+library(httpuv)
 library(httr) # GET, content functions
 
 # --- Famous Quotes ------------------------------------------------------------
@@ -55,7 +73,7 @@ fromJSON(res)
 
 
 # --- ProPublica ---------------------------------------------------------------
-
+# Search ProPublica for public power nonprofits
 search_query <- "PUBLIC%20POWER"
 
 url <- paste0(
@@ -74,8 +92,8 @@ res <- fromJSON(url)
 # Get a login and API key: https://www.quandl.com/?modal=register
 key <- "1hHPXBgH3x_ZA72dC65w"
 
-# Install the Quandl R package:
-install.packages("Quandl")
+# Install the Quandl R package (done above):
+# install.packages("Quandl")
 
 library(Quandl)
 
@@ -177,3 +195,57 @@ library(purrr)
 res <- gridForecast$properties %>%
   data_frame(x = ., name = names(.)) %>%
   pmap(format_grid_forecast)
+
+
+# --- Github -------------------------------------------------------------------
+# Stolen from https://gist.github.com/mGalarnyk/533ccfd1bb46680925448774bcb71647
+
+# Can be github, linkedin etc depending on application
+oauth_endpoints("github")
+
+# Get from https://github.com/settings/applications/
+myapp <- oauth_app(appname = "Access via R",
+                   key = "67a9c8b9b10cc3ca91ab",
+                   secret = "677b83c6b61a222ef0d8ba0ae34ea936197b061d")
+
+# Get a token
+github_token <- oauth2.0_token(oauth_endpoints("github"), myapp)
+
+# Actually start using the API
+gtoken <- config(token = github_token)
+
+# Get all of my repositories
+req <- GET("https://api.github.com/users/srvanderplas/repos", gtoken)
+
+# Check to make sure no errors occurred with the request. 
+# stop_for_status will convert any HTTP errors into R errors
+stop_for_status(req)
+
+# Extract content from the request
+res <- content(req, as = "text")
+github_repos <- fromJSON(res, simplifyDataFrame = T, flatten = T)
+
+# Get only github repositories with associated webpages
+filter(github_repos, has_pages)
+
+# Get the most recently updated repository
+github_repos %>%
+  # Convert updated time to a datetime object
+  mutate(updated_at = ymd_hms(updated_at)) %>%
+  arrange(desc(updated_at)) %>%
+  filter(row_number() == 1)
+
+# See what the most recent commits from NPPD-BIEA-Monthly-Meetings are
+req2 <- GET("https://api.github.com/repos/srvanderplas/NPPD-BIEA-Monthly-Meetings/commits", gtoken)
+stop_for_status(req2)
+
+res2 <- content(req2, as = "text") %>%
+  fromJSON(simplifyDataFrame = T, flatten = T)
+
+# Look at the data frame, but ignore any variable with 
+# "url", "committer", "payload", "signature", or "parents in the name - 
+# too many variables
+select(res2, -matches("url|committer|signature|payload|parents"))
+
+# Look at all commit messages for the repo
+res2$commit.message
